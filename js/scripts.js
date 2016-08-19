bc8e40c79393268329544d428a41df506f68bf69
Alaina Traxler <eloavox@gmail.com>
2016-08-19 14:34:39 -0700

    add basic floor generation and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,60 @@
-var xAxis;
-var yAxis;
+// <span>@@@@@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>
+
+var xAxisLength = 9;
+var yAxisLength = 9;
+var firstFloor = [];
+
+var Room = function() {
+  this.doors = "nsew";
+}
+
+function drawLevel() {
+  for(y = 0; y < yAxisLength; ++y){
+    for(x = 0; x < xAxisLength; ++x){
+
+      // if(y === 0){
+      // $("#" + y + "-" + x).append("<span>@@@@@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+      // }else {
+      // $("#" + y + "-" + x).append("<span>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+      // }
+
+      if(y === 0 && x === 0){
+        $("#" + y + "-" + x).append("<span>@@@@@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+      } else if(y === 0 && x > 0){
+        $("#" + y + "-" + x).append("<span>@@@@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>@@@@</span>");
+      } else if(y > 0 && x === 0){
+        $("#" + y + "-" + x).append("<span>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+      } else{
+        $("#" + y + "-" + x).append("<span>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>@@@@</span>");
+      }
+
+    }
+  }
+}
 
 function buildLevel() {
+  //Builds empty floor
+  // for(x = 0; x < xAxisLength; ++x){
+  //   firstFloor[x] = [];
+  //   for(y = 0; y < yAxisLength; ++y){
+  //     firstFloor[x][y] = x + "-" + y;
+  //   }
+  // }
+
+  for(y = 0; y < yAxisLength; ++y){
+    firstFloor[y] = [];
+    for(x = 0; x < xAxisLength; ++x){
+      firstFloor[y][x] = new Room;
+    }
+  }
+
 
 }
 
 $(document).ready(function() {
   $("#start_button").click(function() {
-
+    $("button").toggle();
     buildLevel();
-
+    drawLevel();
   })
 })
