794fcd682f6648bd0154bbf60cf2a7022a858967
Alaina Traxler <eloavox@gmail.com>
2016-08-19 14:56:25 -0700

    add basic door function and output

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,24 @@ var yAxisLength = 9;
 var firstFloor = [];
 
 var Room = function() {
-  this.doors = "nsew";
+  this.doorN = "|";
+  this.doorS = "|";
+  this.doorE = "-";
+  this.doorW = "-";
 }
 
 function drawLevel() {
   for(y = 0; y < yAxisLength; ++y){
     for(x = 0; x < xAxisLength; ++x){
 
-      // if(y === 0){
-      // $("#" + y + "-" + x).append("<span>@@@@@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
-      // }else {
-      // $("#" + y + "-" + x).append("<span>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
-      // }
-
       if(y === 0 && x === 0){
-        $("#" + y + "-" + x).append("<span>@@@@@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+        $("#" + y + "-" + x).append("<span>@@" + firstFloor[y][x].doorN + "@@<br>@&nbsp;&nbsp;&nbsp;@<br>" + firstFloor[y][x].doorW + "&nbsp;&nbsp;&nbsp;" + firstFloor[y][x].doorE + "<br>@&nbsp;&nbsp;&nbsp;@<br>@@" + firstFloor[y][x].doorS + "@@</span>");
       } else if(y === 0 && x > 0){
-        $("#" + y + "-" + x).append("<span>@@@@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>@@@@</span>");
+        $("#" + y + "-" + x).append("<span>@" + firstFloor[y][x].doorN + "@@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;" + firstFloor[y][x].doorE + "<br>&nbsp;&nbsp;&nbsp;@<br>@" + firstFloor[y][x].doorS + "@@</span>");
       } else if(y > 0 && x === 0){
-        $("#" + y + "-" + x).append("<span>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@&nbsp;&nbsp;&nbsp;@<br>@@@@@</span>");
+        $("#" + y + "-" + x).append("<span>@&nbsp;&nbsp;&nbsp;@<br>" + firstFloor[y][x].doorW + "&nbsp;&nbsp;&nbsp;" + firstFloor[y][x].doorE + "<br>@&nbsp;&nbsp;&nbsp;@<br>@@" + firstFloor[y][x].doorS + "@@</span>");
       } else{
-        $("#" + y + "-" + x).append("<span>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;@<br>@@@@</span>");
+        $("#" + y + "-" + x).append("<span>&nbsp;&nbsp;&nbsp;@<br>&nbsp;&nbsp;&nbsp;" + firstFloor[y][x].doorE + "<br>&nbsp;&nbsp;&nbsp;@<br>@" + firstFloor[y][x].doorS + "@@</span>");
       }
 
     }
@@ -34,12 +31,6 @@ function drawLevel() {
 
 function buildLevel() {
   //Builds empty floor
-  // for(x = 0; x < xAxisLength; ++x){
-  //   firstFloor[x] = [];
-  //   for(y = 0; y < yAxisLength; ++y){
-  //     firstFloor[x][y] = x + "-" + y;
-  //   }
-  // }
 
   for(y = 0; y < yAxisLength; ++y){
     firstFloor[y] = [];
@@ -47,7 +38,6 @@ function buildLevel() {
       firstFloor[y][x] = new Room;
     }
   }
-
 
 }
 
